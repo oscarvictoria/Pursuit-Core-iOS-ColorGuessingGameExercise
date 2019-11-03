9581aa86e936d7ec46da67558ef564a2fdd10336
oscar victoria <oscarvictoriagonzalez@gmail.com>
2019-11-03 17:01:59 -0500

    updated repo

--- ColorGuessingGame/ColorGuessingGame/ColorGuessingGame/ViewController.swift
+++ ColorGuessingGame/ColorGuessingGame/ColorGuessingGame/ViewController.swift
@@ -11,13 +11,16 @@ import UIKit
 class ViewController: UIViewController {
     
     
-@IBOutlet weak var RandomColor: UIView!
+@IBOutlet weak var randomColor: UIView!
 @IBOutlet weak var colorRed: UIButton!
 @IBOutlet weak var colorGreen: UIButton!
 @IBOutlet weak var colorBlue: UIButton!
     
-
+@IBOutlet weak var score: UILabel!
     
+@IBOutlet weak var result: UILabel!
+    
+    @IBOutlet weak var highestScore: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,31 +32,62 @@ class ViewController: UIViewController {
     
     @IBAction func colors(_ sender: UIButton) {
         
-//    let randNum =  CGFloat.random(in: 0...1)
-//    let randNum2 = CGFloat.random(in: 0...1)
-//    let randNum3 = CGFloat.random(in: 0...1)
-//    let randNum4 = CGFloat.random(in: 0...1)
-//
-//    let myColor = UIColor(red: randNum, green: randNum2, blue: randNum3, alpha: randNum4)
+    let randNum =  CGFloat.random(in: 0...1)
+    let randNum2 = CGFloat.random(in: 0...1)
+    let randNum3 = CGFloat.random(in: 0...1)
+    let randNum4 = CGFloat.random(in: 0...1)
+
+    let myColor = UIColor(red: randNum, green: randNum2, blue: randNum3, alpha: randNum4)
+    let array = [ randNum, randNum2, randNum3]
+        let mostColor = array.max()
+
         
-        switch sender {
-        case colorRed:
-            RandomColor.backgroundColor = .red
-        case colorGreen:
-            RandomColor.backgroundColor = .green
-        case colorBlue:
-            RandomColor.backgroundColor = .blue
+    randomColor.backgroundColor = myColor
+        
+       
+  
+        
+        switch sender.tag {
+        case 0:
+            if randNum == mostColor {
+              result.text = "Correct"
+            } else {
+            result.text = "Wrong"
+            }
+        case 1:
+            if randNum2 == mostColor {
+                result.text = "Correct"
+            } else {
+                result.text = "Wrong"
+            }
+        case 2:
+            if randNum3 == mostColor {
+                result.text = "Correct"
+            } else {
+                result.text = "Wrong"
+            }
         default:
-            RandomColor.backgroundColor = .white
+            result.text = "unknown"
         }
-    
         
         
     }
     
+   
+    
     
 
+    
+    
+    
+    
+    
+    @IBAction func newGame(_ sender: UIButton) {
+    }
+    
 
-
+    
+    
+    
 }
 
